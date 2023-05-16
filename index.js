e2f1bf0acf85937f6d5d3227bf18345fd137a65b
flvargas <vargas.flavio@gmail.com>
2023-05-16 14:11:39 -0300

    v1 ok

--- index.js
+++ index.js
@@ -1,44 +1,114 @@
-var img_ele = null,
+var 
+img_ele = null,
 x_cursor = 0,
 y_cursor = 0,
 x_img_ele = 0,
-y_img_ele = 0;
+y_img_ele = 0,
+w_container = document.getElementById("container").getBoundingClientRect().width;
+h_container = document.getElementById("container").getBoundingClientRect().height;
 
 function zoom(zoomincrement) {
     img_ele = document.getElementById('drag-img');
-    var pre_width = img_ele.getBoundingClientRect().width, pre_height = img_ele.getBoundingClientRect().height;
-    img_ele.style.width = (pre_width * zoomincrement) + 'px';
-    img_ele.style.height = (pre_height * zoomincrement) + 'px';
-    img_ele = null;
+
+    var 
+    pre_width = img_ele.getBoundingClientRect().width, 
+    pre_height = img_ele.getBoundingClientRect().height,    
+    zoom_w = Math.round(pre_width * zoomincrement),
+    zoom_h = Math.round(pre_height * zoomincrement);
+
+    if (zoom_w >= w_container && zoom_w < 2000) {
+        img_ele.style.width = zoom_w + 'px';
+        img_ele.style.height = zoom_h + 'px';
+    }
+
+    var
+    x_cursor = window.event.clientX,
+    y_cursor = window.event.clientY,
+    x_pos = x_cursor - x_img_ele,
+    y_pos = y_cursor - y_img_ele,        
+    w_map = img_ele.getBoundingClientRect().width,
+    h_map = img_ele.getBoundingClientRect().height;
+
+    img_ele.style.left = x_pos + 'px';
+    img_ele.style.top = y_pos + 'px';        
+
+    if (x_pos <= Math.round(w_container - w_map)) {            
+        img_ele.style.left = Math.round(w_container - w_map) +'px';
+    }
+    
+    if (y_pos <= Math.round(h_container - h_map)) {
+        img_ele.style.top = Math.round(h_container - h_map) +'px';            
+    }
+
+    if (x_pos >=0) {
+        img_ele.style.left = '0px';
+    }
+
+    if (y_pos >=0) {
+        img_ele.style.top = '0px';
+    }
+
+    
+    img_ele = null;    
 }
 
 document.getElementById('zoomout').addEventListener('click', function() {
-    zoom(0.85);
+    zoom(0.834);
 });
 document.getElementById('zoomin').addEventListener('click', function() {
-    zoom(1.15);
+    zoom(1.2);
 });
 
 function start_drag(e) {
     e.preventDefault();
     img_ele = this;    
     x_img_ele = window.event.clientX - document.getElementById('drag-img').offsetLeft;
-    y_img_ele = window.event.clientY - document.getElementById('drag-img').offsetTop;    
+    y_img_ele = window.event.clientY - document.getElementById('drag-img').offsetTop;        
 }
 
-function stop_drag() {
-    console.log(document.getElementById('drag-img').offsetLeft);
+function stop_drag() {    
     img_ele = null;
 }
 
+function zoom_wheel(e) {    
+    if (e.deltaY == 100) {
+        zoom(0.834);        
+    } else {
+        zoom(1.2);        
+    }
+}
+
 function while_drag() {
-    var x_cursor = window.event.clientX;
-    var y_cursor = window.event.clientY;
-    if (img_ele !== null) {
-        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
-        img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
     
-        //console.log(img_ele.style.left+' - '+img_ele.style.top);
+    if (img_ele !== null) {
+        var
+        x_cursor = window.event.clientX,
+        y_cursor = window.event.clientY,
+        x_pos = x_cursor - x_img_ele,
+        y_pos = y_cursor - y_img_ele,        
+        w_map = img_ele.getBoundingClientRect().width,
+        h_map = img_ele.getBoundingClientRect().height;
+
+        img_ele.style.left = x_pos + 'px';
+        img_ele.style.top = y_pos + 'px';        
+
+        if (x_pos <= Math.round(w_container - w_map)) {            
+            img_ele.style.left = Math.round(w_container - w_map) +'px';
+        }
+        
+        if (y_pos <= Math.round(h_container - h_map)) {
+            img_ele.style.top = Math.round(h_container - h_map) +'px';            
+        }
+
+        if (x_pos >=0) {
+            img_ele.style.left = '0px';
+        }
+
+        if (y_pos >=0) {
+            img_ele.style.top = '0px';
+        }
+
+
 
     }
 }
@@ -46,3 +116,4 @@ function while_drag() {
 document.getElementById('drag-img').addEventListener('mousedown', start_drag);
 document.getElementById('container').addEventListener('mousemove', while_drag);
 document.getElementById('container').addEventListener('mouseup', stop_drag);
+document.getElementById('container').addEventListener('wheel', zoom_wheel);
