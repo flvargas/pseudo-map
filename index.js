ef99ed17919ba475506d40bb4e29133419f6b021
flvargas <vargas.flavio@gmail.com>
2023-05-16 10:34:48 -0300

    Nova Imagem do Mapa

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-alert("oi");
+var img_ele = null,
+x_cursor = 0,
+y_cursor = 0,
+x_img_ele = 0,
+y_img_ele = 0;
+
+function zoom(zoomincrement) {
+    img_ele = document.getElementById('drag-img');
+    var pre_width = img_ele.getBoundingClientRect().width, pre_height = img_ele.getBoundingClientRect().height;
+    img_ele.style.width = (pre_width * zoomincrement) + 'px';
+    img_ele.style.height = (pre_height * zoomincrement) + 'px';
+    img_ele = null;
+}
+
+document.getElementById('zoomout').addEventListener('click', function() {
+    zoom(0.85);
+});
+document.getElementById('zoomin').addEventListener('click', function() {
+    zoom(1.15);
+});
+
+function start_drag(e) {
+    e.preventDefault();
+    img_ele = this;    
+    x_img_ele = window.event.clientX - document.getElementById('drag-img').offsetLeft;
+    y_img_ele = window.event.clientY - document.getElementById('drag-img').offsetTop;    
+}
+
+function stop_drag() {
+    console.log(document.getElementById('drag-img').offsetLeft);
+    img_ele = null;
+}
+
+function while_drag() {
+    var x_cursor = window.event.clientX;
+    var y_cursor = window.event.clientY;
+    if (img_ele !== null) {
+        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
+        img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
+    
+        //console.log(img_ele.style.left+' - '+img_ele.style.top);
+
+    }
+}
+
+document.getElementById('drag-img').addEventListener('mousedown', start_drag);
+document.getElementById('container').addEventListener('mousemove', while_drag);
+document.getElementById('container').addEventListener('mouseup', stop_drag);
